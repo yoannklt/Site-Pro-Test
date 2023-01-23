87c895edd0f6b9d2a6fba46cc558f7bd3cf54d7b
Samantha Schneibel <117281029+SamanthaSchneibel@users.noreply.github.com>
2023-01-23 20:48:42 +0100

    maj

--- components/navbar.php
+++ components/navbar.php
@@ -3,10 +3,10 @@
     <a class="navbar-brand" href="../pages/index.php"><img src="../img/logo.png" alt=""></a>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
-        <a class="nav-link" href="../pages/news.php">News</a>
-        <a class="nav-link" href="../pages/btob.php">Nos expériences</a>
-        <a class="nav-link" href="../pages/about.php">à propos de nous</a>
-        <a class="nav-link" href="../pages/equipements.php">Nos équipements</a>
+        <a class="nav-link text-dark" href="../pages/news.php">News</a>
+        <a class="nav-link text-dark" href="../pages/btob.php">Nos expériences</a>
+        <a class="nav-link text-dark" href="../pages/about.php">à propos de nous</a>
+        <a class="nav-link text-dark" href="../pages/equipements.php">Nos équipements</a>
       </div>
     </div>
   </div>
