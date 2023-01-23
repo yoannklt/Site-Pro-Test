5dba64b848b945c4645f3b679a69dd605ba32559
yoannklt <ykerlogot@gaming.tech>
2023-01-23 15:21:54 +0100

    Un peu de beauté

--- components/navbar.php
+++ components/navbar.php
@@ -1,14 +1,16 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Logo Sence</a>
+    <a class="navbar-brand" href="#">Logo Sense</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
         <a class="nav-link" href="../pages/news.php">News</a>
         <a class="nav-link" href="../pages/btob.php">Nos expériences</a>
         <a class="nav-link" href="../pages/about.php">à propos de nous</a>
         <a class="nav-link" href="../pages/equipements.php">Nos équipements</a>
-        <a class="nav-link" href="#">Connexion</a>
-
+        <?php require_once "../components/signup.php"?>
       </div>
     </div>
   </div>
