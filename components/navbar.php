fcce7de2b442c4f7629791fc3d60fe5f3daa483d
Samantha Schneibel <117281029+SamanthaSchneibel@users.noreply.github.com>
2023-01-23 14:56:20 +0100

    maj

--- components/navbar.php
+++ components/navbar.php
@@ -1,15 +1,12 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Logo Sence</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
-        <a class="nav-link" href="#">News</a>
-        <a class="nav-link" href="#">Nos expériences</a>
-        <a class="nav-link" href="#">à propos de nous</a>
-        <a class="nav-link" href="#">Nos équipements</a>
+        <a class="nav-link" href="../pages/news.php">News</a>
+        <a class="nav-link" href="../pages/btob.php">Nos expériences</a>
+        <a class="nav-link" href="../pages/about.php">à propos de nous</a>
+        <a class="nav-link" href="../pages/equipements.php">Nos équipements</a>
         <a class="nav-link" href="#">Connexion</a>
 
       </div>
