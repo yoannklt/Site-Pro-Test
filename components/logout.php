07e5470d32559b0c8f90b1c12be3082479b66aea
Kerly <115028766+yoannklt@users.noreply.github.com>
2023-01-24 15:55:11 +0100

    on passe en react

--- components/logout.php
+++
@@ -1,3 +0,0 @@
-<form action="../action/logout.php" method="post">
-    <button type="submit" >Se déconnecter</button>
-</form>
--- components/signin.php
+++
@@ -1,7 +0,0 @@
-<form method="post" action="../action/signin.php">
-        <label for="email">Identifiants</label>
-        <input type='email' name='email' placeholder="jackfrost@payetonmail.com"/>
-        <label for="password">Mot de passe</label>
-        <input type='password' name='password' />
-        <button type='submit'>Se connecter</button>
-</form>
