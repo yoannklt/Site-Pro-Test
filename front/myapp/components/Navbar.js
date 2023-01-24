0321f30623f7ae5674568fbbdc99b6f145f9a495
Samantha Schneibel <117281029+SamanthaSchneibel@users.noreply.github.com>
2023-01-24 17:24:24 +0100

    maj

--- front/myapp/components/Navbar.js
+++ front/myapp/components/Navbar.js
@@ -1,35 +1,20 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function CollapsibleExample() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+    <Navbar collapseOnSelect expand="lg" bg="light" variant="light" className="navbar">
       <Container>
-        <Navbar.Brand href="../pages/Home.js">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand className="navbrand" href="../pages/Home.js"><img src="../img/logo.png" alt=""></img></Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
           <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
+            <Nav.Link href="../pages/News.js">News</Nav.Link>
+            <Nav.Link href="../pages/Lightroom.js">Nos expériences</Nav.Link>
+            <Nav.Link href="../pages/About.js">à propos de nous</Nav.Link>
+            <Nav.Link href="../pages/Equipements.js">Nos équipements</Nav.Link>
+            <Nav.Link href="#">Connexion</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
