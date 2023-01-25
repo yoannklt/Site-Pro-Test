1ab9c620ec5b284c1348026520b291d3b389d09b
Samantha Schneibel <117281029+SamanthaSchneibel@users.noreply.github.com>
2023-01-25 17:20:55 +0100

    sqds

--- front/myapp/src/pages/News.js
+++ front/myapp/src/pages/News.js
@@ -9,10 +9,14 @@ function News() {
             <div className="head">
                 <img src={image24} alt="" />
             </div>
-            <div className="container">
-                <img src={image26} alt="" />
-                <h3>évènement : la légende du père noël</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, deserunt saepe? Qui libero similique odit reprehenderit iste nisi eius, consequatur, animi voluptatum laudantium nobis laboriosam possimus sapiente, voluptates aperiam distinctio.</p>
+            <div className="container blocimg">
+                <div className="container">
+                    <img src={image26} alt="" />
+                </div>
+                <div className="container blocgris">
+                    <h3>évènement : la légende du père noël</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, deserunt saepe? Qui libero similique odit reprehenderit iste nisi eius, consequatur, animi voluptatum laudantium nobis laboriosam possimus sapiente, voluptates aperiam distinctio.</p>
+                </div>
             </div>
         </div>
     )
