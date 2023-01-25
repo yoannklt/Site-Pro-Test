48eca8861a5888b660b5676d24d5cc4d8dd8950d
abdelkader-islam-yahia <ayahia@gaming.tech>
2023-01-25 21:34:17 +0100

    la normalement c bon

--- front/myapp/src/pages/News.js
+++ front/myapp/src/pages/News.js
@@ -18,7 +18,7 @@ function News() {
                 
                 <div className="container col col-lg-12">
                     <div className="container blocimg">
-                        <img className="img-fluid" src={image26} alt="img" />
+                        <img className="img-fluid" src={image26} alt="img26" />
                     </div>
                     <div className="container blocgrisnews">
                         <div className="row">
@@ -64,39 +64,11 @@ function News() {
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, deserunt saepe? Qui libero similique odit reprehenderit iste nisi eius, consequatur, animi voluptatum laudantium nobis laboriosam possimus sapiente, voluptates aperiam distinctio. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, deserunt saepe? Qui libero similique odit reprehenderit iste nisi eius, consequatur, animi voluptatum laudantium nobis laboriosam possimus sapiente, voluptates aperiam distinctio. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, deserunt saepe? Qui libero similique odit reprehenderit iste nisi eius, consequatur, animi voluptatum laudantium nobis laboriosam possimus sapiente, voluptates aperiam distinctio.</p>
                     </div>
                 </div>
-
-                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                    <ol class="carousel-indicators">
-                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                    </ol>
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                        <img class="d-block w-100" src={vector9} alt="First slide"></img>
-                        </div>
-                        <div class="carousel-item">
-                        <img class="d-block w-100" src={vector8} alt="Second slide"></img>
-                        </div>
-                        <div class="carousel-item">
-                        <img class="d-block w-100" src={vector10} alt="Third slide"></img>
-                        </div>
-                    </div>
-                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                    </a>
+                <div className="carousel">
+                    <FadeCarousel/>
                 </div>
-            </div>
-            
-            <div className="carousel">
-                <FadeCarousel/>
-            </div>
 
+            </div>
         </div>
     )
 }
