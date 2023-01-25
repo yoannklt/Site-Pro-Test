4052a13a6758a8f1aba747c783ca481a8713c6df
Samantha Schneibel <117281029+SamanthaSchneibel@users.noreply.github.com>
2023-01-25 10:03:39 +0100

    nav

--- front/myapp/src/components/Navbar.js
+++ front/myapp/src/components/Navbar.js
@@ -1,26 +1,26 @@
-import '../index.css'; 
-import Container from 'react-bootstrap/Container';
+import '../index.css';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Logo from '../img/Logo.png';
 
 function Navbarbar() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="light" variant="light" className="navbar">
-      <Container>
-        <Navbar.Brand className="navbrand" href="../pages/Home.js"><img src="../img/logo.png" alt=""></img></Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Container>
-            <Nav>
-              <Nav.Link className='buttons' href="../pages/News">News</Nav.Link>
-              <Nav.Link className='buttons' href="../pages/Lightroom">Nos expériences</Nav.Link>
-              <Nav.Link className='buttons' href="../pages/About">à propos de nous</Nav.Link>
-              <Nav.Link className='buttons' href="../pages/Equipements">Nos équipements</Nav.Link>
-              <Nav.Link className='buttons' href="#"><strong>Connexion</strong></Nav.Link>
-            </Nav>
-          </Container>
-        </Navbar.Collapse>
-      </Container>
+      <div className='col-lg-1 row'>
+        <Nav.Link className="navbrand col-lg-1" href="../pages/Home"><img src={Logo} alt="" /></Nav.Link>
+      </div>
+      <Navbar.Toggle className="test" aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav" className='list col-lg-10'>
+        <Nav.Link className="navbtn" href="../pages/News">News</Nav.Link>
+        <span>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
+        <Nav.Link className="navbtn" href="../pages/Lightroom">Nos expériences</Nav.Link>
+        <span>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
+        <Nav.Link className="navbtn" href="../pages/About">à propos de nous</Nav.Link>
+        <span>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
+        <Nav.Link className="navbtn" href="../pages/Equipements">Nos équipements</Nav.Link>
+        <span>&nbsp;&nbsp;|&nbsp;&nbsp;</span>
+        <Nav.Link className="navbtn" href="#"><strong>Connexion</strong></Nav.Link>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
