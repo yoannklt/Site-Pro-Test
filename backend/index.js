9c0741e30bce0abeed233896b0def66749600777
abdel <ayahia@gaming.tech>
2023-01-25 08:38:04 +0100

    CRUD user

--- backend/index.js
+++ backend/index.js
@@ -26,4 +26,69 @@ app.get("/user/list", function (req, res) {
         }
       });
   });
+
+  // INSERER DES UTILISATEUR
+app.post('/user/insert', jsonParser, (req, res) => {
+  const body = req.body;
+  const dbConnect = dbo.getDb();
+  dbConnect
+  .collection("user")
+  .insertOne({...body})
+  .toArray(function (err, result) {
+      if (err) {
+      res.status(400).send("Error fetching pokemons!");
+      } else {
+      res.json(result);
+      }
+  });
   
+  res.json("c'est inserer");
+});
+
+// SUPPRIMER LES UTILISATEUR
+app.post('/user/delete', jsonParser, (req, res) =>{
+  const body = req.body;
+  const dbConnect = dbo.getDb();
+  const doc = {
+      name:body.name
+  };
+  dbConnect
+      .collection("user")
+      .deleteOne(doc)
+      .then(function (result,err) {
+          if (err) {
+              res.status(400).send("ok");
+          }else{
+              res.json(result)
+          }
+      })
+      
+      res.json("c'est supprimer");
+  });
+
+// MODIFIER LES UTILISATEUR
+app.post('/user/updateUser', jsonParser, (req, res) =>{
+  const body = req.body;
+  const dbConnect = dbo.getDb();
+  dbConnect
+  .collection("user")
+  .updateOne({
+      name: body.prevname
+  },{
+      $set:{ 
+          name: body.newname
+      }
+  })
+  .toArray(function (err, result) {
+      if (err) {
+      res.status(400).send("Error fetching pokemons!");
+      } else {
+      res.json(result);
+      }
+  });
+
+  res.json("c'est modifier");
+});
+
+
+
