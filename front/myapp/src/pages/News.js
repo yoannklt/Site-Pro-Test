c54b12a6f4e853b2806cd8cd59ef55f5cf11dd6f
Samantha Schneibel <117281029+SamanthaSchneibel@users.noreply.github.com>
2023-01-25 18:02:38 +0100

    azerty

--- front/myapp/src/pages/News.js
+++ front/myapp/src/pages/News.js
@@ -1,21 +1,63 @@
 import Navbarbar from "../components/Navbar"
 import image24 from '../img/image24.png';
 import image26 from '../img/image26.png';
+import image25 from '../img/image25.png';
 
 function News() {
     return (
         <div>
             <Navbarbar />
-            <div className="head">
-                <img src={image24} alt="" />
-            </div>
-            <div className="container blocimg">
-                <div className="container">
-                    <img src={image26} alt="" />
+            <div className="">
+                <div className="head">
+                    <img className="img-fluid" src={image24} alt="" />
                 </div>
-                <div className="container blocgris">
-                    <h3>évènement : la légende du père noël</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, deserunt saepe? Qui libero similique odit reprehenderit iste nisi eius, consequatur, animi voluptatum laudantium nobis laboriosam possimus sapiente, voluptates aperiam distinctio.</p>
+                <div className="container col col-lg-12">
+                    <div className="container blocimg">
+                        <img className="img-fluid" src={image26} alt="" />
+                    </div>
+                    <div className="container blocgris">
+                        <div className="row">
+                        </div>
+                        <h3><strong>évènement : la légende du père noël</strong></h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, deserunt saepe? Qui libero similique odit reprehenderit iste nisi eius, consequatur, animi voluptatum laudantium nobis laboriosam possimus sapiente, voluptates aperiam distinctio. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, deserunt saepe? Qui libero similique odit reprehenderit iste nisi eius, consequatur, animi voluptatum laudantium nobis laboriosam possimus sapiente, voluptates aperiam distinctio. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, deserunt saepe? Qui libero similique odit reprehenderit iste nisi eius, consequatur, animi voluptatum laudantium nobis laboriosam possimus sapiente, voluptates aperiam distinctio.</p>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="container col-lg-6">
+                        <div className="container blocimg">
+                            <img className="img-fluid" src={image25} alt="" />
+                        </div>
+                        <div className="container blocgris">
+                            <div className="row">
+                            </div>
+                            <h3><strong>évènement : le mystère du loup pharaon.</strong></h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, deserunt saepe? Qui libero similique odit reprehenderit iste nisi eius, consequatur, animi voluptatum laudantium nobis laboriosam possimus sapiente, voluptates aperiam distinctio. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, deserunt saepe? Qui libero similique odit reprehenderit iste nisi eius, consequatur, animi voluptatum laudantium nobis laboriosam possimus sapiente, voluptates aperiam distinctio. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, deserunt saepe? Qui libero similique odit reprehenderit iste nisi eius, consequatur, animi voluptatum laudantium nobis laboriosam possimus sapiente, voluptates aperiam distinctio.</p>
+                        </div>
+                    </div>
+                    <div className="container col-lg-6">
+                        <div className="container blocimg">
+                            <img className="img-fluid" src={image25} alt="" />
+                        </div>
+                        <div className="container blocgris">
+                            <div className="row">
+                            </div>
+                            <h3><strong>évènement : le mystère du loup pharaon.</strong></h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, deserunt saepe? Qui libero similique odit reprehenderit iste nisi eius, consequatur, animi voluptatum laudantium nobis laboriosam possimus sapiente, voluptates aperiam distinctio. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, deserunt saepe? Qui libero similique odit reprehenderit iste nisi eius, consequatur, animi voluptatum laudantium nobis laboriosam possimus sapiente, voluptates aperiam distinctio. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, deserunt saepe? Qui libero similique odit reprehenderit iste nisi eius, consequatur, animi voluptatum laudantium nobis laboriosam possimus sapiente, voluptates aperiam distinctio.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="container col-lg-12">
+                    <div className="container blocimg">
+                        <img className="img-fluid" src={image25} alt="" />
+                    </div>
+                    <div className="container blocgris">
+                        <div className="row">
+                        </div>
+                        <h3><strong>évènement : le mystère du loup pharaon.</strong></h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, deserunt saepe? Qui libero similique odit reprehenderit iste nisi eius, consequatur, animi voluptatum laudantium nobis laboriosam possimus sapiente, voluptates aperiam distinctio. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, deserunt saepe? Qui libero similique odit reprehenderit iste nisi eius, consequatur, animi voluptatum laudantium nobis laboriosam possimus sapiente, voluptates aperiam distinctio. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, deserunt saepe? Qui libero similique odit reprehenderit iste nisi eius, consequatur, animi voluptatum laudantium nobis laboriosam possimus sapiente, voluptates aperiam distinctio.</p>
+                    </div>
                 </div>
             </div>
         </div>
