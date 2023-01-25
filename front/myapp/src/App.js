7f1fb5def3282f26ca292d71ceeaab5f526922da
Samantha Schneibel <117281029+SamanthaSchneibel@users.noreply.github.com>
2023-01-25 16:27:11 +0100

    azerty

--- front/myapp/src/App.js
+++ front/myapp/src/App.js
@@ -1,5 +1,13 @@
 import "./index.css";
 import Home from "./pages/Home";
+import News from "./pages/News";
+import About from "./pages/About";
+import Btob from "./pages/Btob";
+import Lightroom from "./pages/Lightroom";
+import Darkroom from "./pages/Darkroom";
+import Equipements from "./pages/Equipements";
+import Creativeroom from "./pages/Creativeroom";
+import Battleroom from "./pages/Battleroom";
 import {
   BrowserRouter as Router,
   Switch,
@@ -12,6 +20,30 @@ function App(props){
       <Switch>
         <Route exact path="/"> {/*ici on met l'URL dans le navigateur*/}
           <Home /> {/*ici on donne la page à afficher en fonction de cette URL*/}
+        </Route>
+        <Route exact path="/News"> {/*ici on met l'URL dans le navigateur*/}
+          <News /> {/*ici on donne la page à afficher en fonction de cette URL*/}
+        </Route>
+        <Route exact path="/About"> {/*ici on met l'URL dans le navigateur*/}
+          <About /> {/*ici on donne la page à afficher en fonction de cette URL*/}
+        </Route>
+        <Route exact path="/Btob"> {/*ici on met l'URL dans le navigateur*/}
+          <Btob /> {/*ici on donne la page à afficher en fonction de cette URL*/}
+        </Route>
+        <Route exact path="/Lightroom"> {/*ici on met l'URL dans le navigateur*/}
+          <Lightroom /> {/*ici on donne la page à afficher en fonction de cette URL*/}
+        </Route>
+        <Route exact path="/Darkroom"> {/*ici on met l'URL dans le navigateur*/}
+          <Darkroom /> {/*ici on donne la page à afficher en fonction de cette URL*/}
+        </Route>
+        <Route exact path="/Equipements"> {/*ici on met l'URL dans le navigateur*/}
+          <Equipements /> {/*ici on donne la page à afficher en fonction de cette URL*/}
+        </Route>
+        <Route exact path="/Creativeroom"> {/*ici on met l'URL dans le navigateur*/}
+          <Creativeroom /> {/*ici on donne la page à afficher en fonction de cette URL*/}
+        </Route>
+        <Route exact path="/Battleroom"> {/*ici on met l'URL dans le navigateur*/}
+          <Battleroom /> {/*ici on donne la page à afficher en fonction de cette URL*/}
         </Route>
       </Switch>
   </Router>
