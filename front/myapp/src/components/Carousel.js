5419de9eda7e1d717e6d86cefa20b5e63bdf6165
Samantha Schneibel <117281029+SamanthaSchneibel@users.noreply.github.com>
2023-01-26 17:15:56 +0100

    car

--- front/myapp/src/components/Carousel.js
+++ front/myapp/src/components/Carousel.js
@@ -10,62 +10,37 @@ function FadeCarousel() {
     return (
 
         <div className='carousel'>
-            <Carousel
-                fade
-                pause hover
-            >
+            <Carousel fade pause hover>
                 <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={image23}
-                        alt="First slide"
-                    />
-                    <div className='black'>
+                    <img className="d-block w-100" src={image23} alt=""/>
                         <Carousel.Caption>
                             <p>“C'est dingue, j'ai vraiment eu l'impression d'être transportée dans un autre monde. Avant je ne faisais pas d'expérience VR car je ne croyais pas en la qualité mais grâce à The Sense, j'ai pu traverser la frontière du réel.”</p>
                             <p>- Denise, 23 Octobre 2020 -</p>
                         </Carousel.Caption>
-                    </div>
                 </Carousel.Item>
                 <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={image22}
-                        alt="Second slide"
-                    />
+                    <img className="d-block w-100" src={image22} alt=""/>
                     <Carousel.Caption>
                         <h3>les équipements the sense</h3>
                         <p>Tous nos équipements sont prévus pour toutes les tailles et tous les âges, ils conviennent aussi bien aux adultes qu'aux jeunes enfants de 12ans. Ils vous garantissent un confort à toutes épreuves lors de vos voyages chez nous.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={image21}
-                        alt="Third slide"
-                    />
+                    <img className="d-block w-100" src={image21} alt=""/>
                     <Carousel.Caption>
                         <h3>Nouvelle room en vue !</h3>
                         <p>En 2021, préparez-vous à manger du slime en pleine face, les fantômes de Ghost Buster arrivent chez THE SENSE. Cela promet de belles parties de chasse aux fantômes dans tout le complexe. Les réservations sont d'ores et déjà possibles sur place et en Février sur internet.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <CarouselItem>
-                    <img
-                        className="d-block w-100"
-                        src={image20}
-                        alt="Fourth slide"
-                    />
+                    <img className="d-block w-100" src={image20} alt=""/>
                     <Carousel.Caption>
                         <p>“Avec mes fils nous avons tenté l'expérience “NAMELESS”, moi qui pensais avoir tout vu dans le domaine de l'horreur, je ne me suis jamais autant trompée. Si vous êtes à la recherche de sensations fortes et de frissons, la DARK ROOM est faite pour vous.”</p>
                         <p>- Nicolas, 3 Septembre 2020 -</p>
                     </Carousel.Caption>
                 </CarouselItem>
                 <CarouselItem>
-                    <img
-                        className="d-block w-100"
-                        src={image27}
-                        alt="Fifth slide"
-                    />
+                    <img className="d-block w-100" src={image27} alt=""/>
                     <Carousel.Caption>
                         <p>“C'est dingue, j'ai vraiment eu l'impression d'être transporté dans un autre monde. Avant je ne faisais pas d'expérience VR car je ne croyais pas en la qualité mais grâce à The Sense, j'ai pu traverser la frontière du réel.”</p>
                         <p>- Denise, 23 Octobre 2020 -</p>
