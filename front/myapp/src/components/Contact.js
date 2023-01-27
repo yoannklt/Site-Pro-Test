0a1fa05b0524050a73ee0d185a88dc85b49bd5e2
abdel <ayahia@gaming.tech>
2023-01-27 16:03:00 +0100

    Btob et contact avance

--- front/myapp/src/components/Contact.js
+++ front/myapp/src/components/Contact.js
@@ -1,95 +1,45 @@
-<section class="mb-4">
-
-    {/* <!--Section heading--> */}
-    <h2 class="h1-responsive font-weight-bold text-center my-4">Contact us</h2>
-    {/* <!--Section description--> */}
-    <div class="row">
-        {/* <!--Grid column--> */}
-        <div class="col-md-9 mb-md-0 mb-5">
-            <form id="contact-form" name="contact-form" action="mail.php" method="POST">
-                {/* <!--Grid row--> */}
-                <div class="row">
-                    {/* <!--Grid column--> */}
-                    <div class="col-md-6">
-                        <div class="md-form mb-0">
-                            <input type="text" id="name" name="name" class="form-control"></input>
-                            <label for="name" class="">Votre nom</label>
+function Contactee (){
+    return(
+        <section class="mb-4">
+            <h2 class="h1-responsive font-weight-bolder text-center my-4 ">CONTACTEZ NOUS</h2>
+            <div class="row formes">
+                <div class="col-md-9 mb-md-0 mb-5">
+                    <form id="contact-form" name="contact-form" action="mail.php" method="POST">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="row prenom">
+                                    <div class="md-form mb-1">
+                                        <input type="text" id="name" name="name" class="form-control" placeholder="Votre nom"/>
+                                    </div>
+                                    <div class="md-form mb-1">
+                                        <input type="text" id="subject" name="subject" class="form-control" placeholder="Votre prénom"/>
+                                    </div>
+                                </div>
+                                
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="md-form mb-0">
-                            <input type="text" id="name" name="name" class="form-control"></input>
-                            <label for="name" class="">Votre Prénom</label>
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="md-form mb-0">
+                                    <input type="text" id="email" name="email" class="form-control" placeholder="Votre email"/>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    {/* <!--Grid column--> */}
-
-                    {/* <!--Grid column--> */}
-                    <div class="col-md-6">
-                        <div class="md-form mb-0">
-                            <input type="text" id="email" name="email" class="form-control"></input>
-                            <label for="email" class="">Votre Email</label>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="md-form">
+                                    <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea" placeholder="Votre projet"></textarea>
+                                </div>
+                            </div>
                         </div>
+                    </form>
+                    <div class="text-center text-md-left">
+                        <a class="btn btn-primary" onclick="document.getElementById('contact-form').submit();">Send</a>
                     </div>
-                    {/* <!--Grid column--> */}
-
+                    <div class="status"></div>
                 </div>
-                {/* <!--Grid row--> */}
-
-                {/* <!--Grid row--> */}
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="md-form mb-0">
-                            <input type="text" id="subject" name="subject" class="form-control"></input>
-                            <label for="subject" class="">Votre Projet</label>
-                        </div>
-                    </div>
-                </div>
-                {/* <!--Grid row--> */}
-
-                {/* <!--Grid row--> */}
-                <div class="row">
-
-                    {/* <!--Grid column--> */}
-                    <div class="col-md-12">
-
-                        <div class="md-form">
-                            <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
-                            <label for="message">Your message</label>
-                        </div>
-
-                    </div>
-                </div>
-                {/* <!--Grid row--> */}
-
-            </form>
-
-            <div class="text-center text-md-left">
-                <a class="btn btn-primary" onclick="document.getElementById('contact-form').submit();">Send</a>
             </div>
-            <div class="status"></div>
-        </div>
-        {/* <!--Grid column--> */}
-
-        {/* <!--Grid column--> */}
-        <div class="col-md-3 text-center">
-            <ul class="list-unstyled mb-0">
-                <li><i class="fas fa-map-marker-alt fa-2x"></i>
-                    <p>San Francisco, CA 94126, USA</p>
-                </li>
-
-                <li><i class="fas fa-phone mt-4 fa-2x"></i>
-                    <p>+ 01 234 567 89</p>
-                </li>
-
-                <li><i class="fas fa-envelope mt-4 fa-2x"></i>
-                    <p>contact@mdbootstrap.com</p>
-                </li>
-            </ul>
-        </div>
-        {/* Grid column */}
-
-    </div>
-
-</section>
-{/* <!--Section: Contact v.2--> */}
+        </section>
+    );
+}
+export default Contactee;
