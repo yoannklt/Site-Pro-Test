4ad998257b0035bfdb91dd302ea01360ea4ebc7e
Skynezzz <117281599+Skynezzz@users.noreply.github.com>
2023-01-31 18:38:53 +0100

    sheesh

--- front/myapp/src/pages/Room.js
+++ front/myapp/src/pages/Room.js
@@ -2,11 +2,21 @@
 import Lightroom from "../components/Lightroom";
 import Darkroom from "../components/Darkroom";
 import Battleroom from "../components/Battleroom";
+import { useContext } from "react";
+import { RoomContext } from "../context/RoomContext";
 
 
 function Room() {
+
+    const { roomContext, setRoomContext } = useContext(RoomContext);
+
     return (
-        <Lightroom />
+        <>
+            {(roomContext == 'lightRoom') ? <Lightroom /> 
+            : (roomContext == 'darkRoom') ? <Darkroom /> 
+            : <Battleroom />}
+        </>
+
     )
 }
 
