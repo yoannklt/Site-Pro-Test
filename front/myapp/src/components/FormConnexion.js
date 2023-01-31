648e72ad0bd6c7be12b3825ccfc3d820d19abbf9
Skynezzz <117281599+Skynezzz@users.noreply.github.com>
2023-01-31 15:33:22 +0100

    dsiyfhiusdhf

--- front/myapp/src/components/FormConnexion.js
+++ front/myapp/src/components/FormConnexion.js
@@ -46,28 +46,7 @@ function FormConnexion(props) {
                     <Form.Control className="fout text-end" type="submit" value="Connexion" />
                 </div>
             </Form>
-            ):( 
-                <h2>{user.first_name}</h2>
-            // <Form className="row fromCoo align-items-center align-self-center" onSubmit={handleSubmit(onSubmit)}>
-            //         <Form.Label className="labelLog col-lg-12 oppai">
-            //             <strong>{user.first_name}</strong>
-            //         </Form.Label>
-            //         <Form.Label className="col-lg-4 text-end">
-            //             Identifiant&nbsp;&nbsp;
-            //         </Form.Label>
-            //         <Form.Input className="col-lg-8 pout" {...register("email")} type="email" placeholder="Email" />
-            //         <Form.Label className="col-lg-4 text-end">
-            //             Mot de passe&nbsp;&nbsp;
-            //         </Form.Label>
-            //         <Form.Input className="col-lg-8 pout" {...register("password")} type="password" placeholder="Mot de passe" />
-            //         <div className="col-lg-12 userButton labelLog">
-            //             <Form.Label>
-            //                 <Link to="" >Créer un compte</Link>&nbsp;&nbsp;
-            //             </Form.Label>
-            //             <Form.Input className="fout" type="submit" value="Connexion" />
-            //         </div>
-            // </Form>
-            )
+            ):null
         } 
 
         </div>
