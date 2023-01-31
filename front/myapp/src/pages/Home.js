47b050c8bf0dde8ed3831d9432d40ee13a1343ee
Samantha Schneibel <117281029+SamanthaSchneibel@users.noreply.github.com>
2023-01-31 14:14:58 +0100

    room

--- front/myapp/src/pages/Home.js
+++ front/myapp/src/pages/Home.js
@@ -1,5 +1,4 @@
 import "../css/Home.css";
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbarbar from "../components/Navbar";
 import image9 from "../img/image9.png";
@@ -48,11 +47,7 @@ function Home() {
                     </div>
                     <div className="col-lg-7 txt row align-items-baseline">
                         <div className="col col-lg-4 text-end h1Home">
-                            {!user ? (
                                 <h1 className="">Qu'est-ce que</h1>
-                            ) : (
-                                <h1 className="">Qu'est-ce que {user.first_name}</h1>
-                            )}
                         </div>
                         <div className="col col-lg-2">
                             <img className="img-fluid logoHome text-start" src={thesense} alt="logo the sense"></img>
