6248eeb6f084d39c17ff09e24bf0d39f77ff2df8
Skynezzz <117281599+Skynezzz@users.noreply.github.com>
2023-01-31 13:21:26 +0100

    connections presque fonctionnelles

--- front/myapp/src/pages/Home.js
+++ front/myapp/src/pages/Home.js
@@ -15,9 +15,17 @@ import Reservation from "../components/Reservation";
 import FadeCarouselavis from "../components/Carouselavis";
 import BasicExample from "../components/Dropdown";
 import Footerter from "../components/Footer";
+import { useContext } from "react";
+import { UserContext } from "../context/UserContext";
 
 
 function Home() {
+    const { user, setUser } = useContext(UserContext);
+
+    const userToken = JSON.parse(sessionStorage.getItem('token'))
+    if (userToken && !user) {
+        setUser(userToken);
+    }
     return (
         <div>
             <div>
@@ -38,7 +46,11 @@ function Home() {
                     </div>
                     <div className="col-lg-7 txt row align-items-baseline">
                         <div className="col col-lg-4 text-end h1Home">
-                            <h1 className="">Qu'est-ce que</h1>
+                            {!user ? (
+                                <h1 className="">Qu'est-ce que</h1>
+                            ) : (
+                                <h1 className="">Qu'est-ce que {user.first_name}</h1>
+                            )}
                         </div>
                         <div className="col col-lg-2">
                             <img className="img-fluid logoHome text-start" src={thesense} alt="logo the sense"></img>
