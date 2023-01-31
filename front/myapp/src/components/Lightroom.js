8805d39e29c3ee27ad4c8907361e6bd5d909928f
Samantha Schneibel <117281029+SamanthaSchneibel@users.noreply.github.com>
2023-01-31 14:32:40 +0100

    roiom

--- front/myapp/src/components/Lightroom.js
+++ front/myapp/src/components/Lightroom.js
@@ -1,4 +1,13 @@
 import "../css/Room.css";
+import Navbarbar from "../components/Navbar";
+import image73 from "../img/image73.png";
+import thesense from '../img/thesense.png';
+import image10 from "../img/image10.jpg";
+import image14 from "../img/image14.png";
+import boutonréserver from "../img/boutonréserver.png";
+import FadeCarousel from "../components/Carousel";
+import Footerter from "../components/Footer";
+
 
 function Lightroom() {
     return (
@@ -17,11 +26,11 @@ function Lightroom() {
 
                 <div className="row bandeaubleu" >
                     <div className="col-lg-4 img text-center">
-                        <img className="img-fluid imgronde align-self-center" src={image9} alt="img9"></img>
+                        <img className="img-fluid imgronde align-self-center" src={image73} alt="img9"></img>
                     </div>
                     <div className="col-lg-7 txt row align-items-baseline">
                         <div className="col col-lg-4 text-end h1Home">
-                                <h1 className="">Qu'est-ce que la</h1>
+                            <h1 className="">Qu'est-ce que la</h1>
                         </div>
                         <div className="col col-lg-2">
                             <img className="img-fluid logoHome text-start" src={thesense} alt="logo the sense"></img>
@@ -33,15 +42,12 @@ function Lightroom() {
                 </div>
 
                 <div className="container description col-lg-12 align-text-center">
-                    <h2><strong>La réalité à portée de main</strong></h2>
-                    <p>Vous rêvez de voyager, de frissoner ou tout simplement de vivre une expérience unique ? Explorez nos univers entre amis ou en famille et franchissez la frontière de la réalité. Plusieurs dimensions d'offrent à vous, vous donnant accès à de nombreuses expériences.</p>
+                    <h2><strong>Prenez part au voyage</strong></h2>
                 </div>
 
                 <div className="container col col-lg-12">
                     <div className="container blocimg">
-                        <h3><strong>Nos expériences les plus appréciées</strong></h3>
                         <div>
-
                             <img className="img-fluid" src={image10} alt="" />
                         </div>
                     </div>
@@ -55,24 +61,38 @@ function Lightroom() {
                             <p>Shangri-La la cité mythique, symbole de la paix, de prospérité et de magnficence. Personne n'a apparemment pu se rendre en ce lieu depuis des décennies ou prouver son existance, du moins depuis votre découverte ! Aventurez-vous au plus profond des légendes, entrez dans la cité de Z avec votre équipe et explorez les lieux à la recherche de Percy Fawcett.</p>
                         </div>
                         <div className="row reservebtn text-center">
-                            <div className="clc col-lg-6 align-self-center">
+                            <div className="clc col-lg-12 align-self-center">
                                 <a href="/Room"><img src={boutonréserver} alt="" /></a>
-                            </div>
-                            <div className="redirection col-lg-2 align-items-end">
-                                <a href="/Room">découvrez la Light room -</a>
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <div className="avis">
-                    <div className="container">
-                        <h2><strong>Qu'est-ce qui vous retiens ?</strong></h2>
+                <div className="container col col-lg-12">
+                    <div className="container blocimg">
+                        <div>
+                            <img className="img-fluid" src={image14} alt="" />
+                        </div>
                     </div>
+                    <div className="container blocgris">
+                        <div className="blochome">
+                            <h3><strong>Nordrënn : la légende de glace</strong></h3>
+                            <h3>|</h3>
+                            <h3 className="title">light room</h3>
+                        </div>
+                        <div className="row parag">
+                            <p>Dans le froid du royaume de Nordrënn, il est une légende qui raconte coimment un guerrier abtint la force de l'ours et la claivoyance du corbeau. il est dit que pour conquérir la femme qu'il aimait, cet homme parti à la recherce du trône d'Odin, artefact perdu depuis des années, qui offrait, disait-on, le pouvoir du Père de Toute Chose. Le guerrier parti et ne revint jamais : on raconte qu'il aurait trouvé le trône mais, qu'avide de son pouvoir il ne le quittera plus. Partez à la découverte du royaume glacé de Nordrënn et retrouvez le guerrier de la légende.</p>
+                        </div>
+                        <div className="row reservebtn text-center">
+                            <div className="clc col-lg-12 align-self-center">
+                                <a href="/Room"><img src={boutonréserver} alt="" /></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
-                    <div className="carouselavis">
-                        <FadeCarouselavis />
-                    </div>
+                <div className="carousel">
+                    <FadeCarousel />
                 </div>
 
             </div>
--- front/myapp/src/pages/Room.js
+++ front/myapp/src/pages/Room.js
@@ -1,14 +1,10 @@
-import FadeCarousel from "../components/Carousel";
-import Footerter from "../components/Footer";
-import Navbarbar from "../components/Navbar";
-import ScrollButton from "../components/ScrollButton";
 import Lightroom from "../components/Lightroom";
 import Darkroom from "../components/Darkroom";
 
 
 function Room() {
     return (
-        <Lightroom />
+        <Darkroom />
     )
 }
 
