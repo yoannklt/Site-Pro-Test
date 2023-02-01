876014c26ebed9a7eecb6daa5009ae6bbce43cf9
Skynezzz <117281599+Skynezzz@users.noreply.github.com>
2023-02-01 08:37:14 +0100

    ouidsfiodsf

--- front/myapp/src/pages/Room.js
+++ front/myapp/src/pages/Room.js
@@ -1,31 +1,60 @@
-
 import Lightroom from "../components/Lightroom";
 import Darkroom from "../components/Darkroom";
 import Battleroom from "../components/Battleroom";
 import { useContext } from "react";
 import { RoomContext } from "../context/RoomContext";
+import lightroom from "../img/lightroom.png"
+import darkroom from "../img/darkroom.png"
+import battleroom from "../img/battleroom.png"
 
 
 function Room() {
 
-    const {room, setRoom} = useContext(RoomContext)
-    
+    const { room, setRoom } = useContext(RoomContext)
+
+
+    const setLightroom = () => {
+        setRoom('Lightroom');
+    }
+
+    const setDarkroom = () => {
+        setRoom('Darkroom');
+    }
+
+    const setBattleroom = () => {
+        setRoom('Battleroom');
+    }
+
+
+
+    console.log(room)
+
     return (
         <>
-        {room ? (
-            <div className="col-lg-12 text-center align-items-center">
-                <button>Battle Room</button>
-                <button>Battle Room</button>
-                <button>Battle Room</button>  
+
+            <div className="col-lg-12 text-center align-items-center mt-5">
+                <img src={lightroom} onClick={setLightroom}></img>
+                <img src={darkroom} onClick={setDarkroom}></img>
+                <img src={battleroom} onClick={setBattleroom}></img>
             </div>
-        ):(
-            <div>
-                <button>Battle Room</button>
-                <button>Battle Room</button>
-                <button>Battle Room</button>  
-            </div>
-        )}
-        
+            {(!room) ? (
+                <Lightroom />
+            ) : (
+                (room === 'Lightroom') ? (
+                    <Lightroom />
+                ) : (
+                    (room === 'Darkroom') ? (
+                        <Darkroom />
+                    ) : (
+                        (room === 'Battleroom') ? (
+                            <Battleroom />
+                        ) : (
+                            <Lightroom />
+                        )
+                    )
+
+                )
+            )}
         </>
     )
 }
