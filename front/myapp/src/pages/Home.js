594eb684f97ee46474deb68b4b7664b74f6f4604
Samantha Schneibel <117281029+SamanthaSchneibel@users.noreply.github.com>
2023-02-01 14:11:13 +0100

    home

--- front/myapp/src/pages/Home.js
+++ front/myapp/src/pages/Home.js
@@ -29,7 +29,7 @@ function Home() {
     if (userToken && !user) {
         setUser(userToken);
     }
-    
+
     return (
         <div>
             <div>
@@ -45,7 +45,18 @@ function Home() {
                 </div>
 
                 <div className="row bandeaubleu" >
-                    <Bandeau />
+                    <div className="row bandeaubleu">
+                        <div className="col-lg-4 img text-center">
+                            <img className="img-fluid imgronde align-self-center" src={image9} alt="img9"></img>
+                        </div>
+                        <div className="col-lg-7 txt row align-items-center textebottom">
+                            <h3 className="">Qu'est-ce que
+                                <img className="img-fluid logoHome text-start" src={thesense} alt="logo the sense" />
+                                &nbsp;?
+                            </h3>
+                            <p>Préparez-vous pour une expérience unique qui vous emmenera dans un autre univers. Vivez vos émotions comme vous ne l'avez jamais fait auparavant. Avec THE SENSE explorez d’autres dimensions et vivez l’impossible en interragissant avec un environnement dynamique et virtuel. Ce n’est pas une expérience en réalité virtuelle que vous vivez, c’est la réalité.</p>
+                        </div>
+                    </div>
                 </div>
 
                 <div className="container description col-lg-12 align-text-center">
@@ -161,7 +172,7 @@ function Home() {
                 </div>
 
                 <div id="reservation">
-                    
+
                 </div>
 
 
