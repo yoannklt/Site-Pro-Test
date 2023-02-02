5ab21a1864358e6f6cf3dbaa5daa43ce96ca4c66
Samantha Schneibel <117281029+SamanthaSchneibel@users.noreply.github.com>
2023-02-02 21:06:21 +0100

    final

--- front/myapp/src/components/Bandeau.js
+++ front/myapp/src/components/Bandeau.js
@@ -8,7 +8,7 @@ function Bandeau(props) {
             <div className="col-lg-4 img text-center">
                 <img className="img-fluid imground align-self-center" src={props.image} alt="The Sense VR AR THESENSE TheSense"></img>
             </div>
-            <div className="col-lg-7 row align-items-center textbottom">
+            <div className={"col-lg-7 row align-items-center textbottom " + props.theme}>
                 <h3 className="">{props.title}
                     <img className="img-fluid logoHome text-start" src={props.logo} alt="The Sense VR AR THESENSE TheSense" />
                     &nbsp;?
